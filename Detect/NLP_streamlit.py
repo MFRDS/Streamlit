4044b5e363e60bfa275cd41033bba3d85ae6bbaa
MFRDS <91241592+MFRDS@users.noreply.github.com>
2023-06-06 14:07:03 +0700

    Update NLP_streamlit.py

--- Detect/NLP_streamlit.py
+++ Detect/NLP_streamlit.py
@@ -1,99 +1,117 @@
+# Menginstall packages yang dibutuhkan
+!pip install gensim numpy pandas sastrawi scikit-learn
 import pandas as pd
 import numpy as np
-import nltk
-nltk.download('punkt')
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from sklearn.preprocessing import LabelEncoder
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn import model_selection, naive_bayes, svm
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
-from nltk import ngrams
-import streamlit as st
+from gensim.models import Word2Vec
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
-# Random seed for consistency
-np.random.seed(500)
-
-# Uncomment line below to download nltk requirements
-# nltk.download('punkt')
-factory = StemmerFactory()
-stemmer = factory.create_stemmer()
-Encoder = LabelEncoder()
-Tfidf_vect = TfidfVectorizer()
+# Mengubah narasi menjadi bentuk yang telah di-stem dan tidak mengandung stopword
+# Mengambil dua kata yang sama namun memiliki arti yang berbeda secara acak untuk setiap narasi
+# Inisialisasi stopword remover dan stemmer
+import random
+stopword_factory = StopWordRemoverFactory()
+stopwords = stopword_factory.get_stop_words()
+stemmer_factory = StemmerFactory()
+stemmer = stemmer_factory.create_stemmer()
+stopword_remover = stopword_factory.create_stop_word_remover()
 
 # Configuration
-DATA_LATIH = "./Detect/Data Latih/DATA_TRAIN.csv"
-DATA_UJI = "./Detect/Data Uji/Data Uji BDC.csv"
+data_train = "./data_train/data_train.csv"
 
 
-def preprocess_text(text):
-    lower = stemmer.stem(text.lower())
-    tokens = word_tokenize(lower)
-    n = 2  # Panjang n-gram yang diinginkan
-    ngram_lower = list(ngrams(tokens, n))
-    ngram_text = " ".join([" ".join(ngram) for ngram in ngram_lower])
-    return ngram_text
+# Mengambil dua kata yang sama namun mengandung beda arti secara acak
+def process_narasi(narasi):
+    # Stemming
+    stemmed_narasi = stemmer.stem(narasi)
+
+    # Menghapus stopword
+    clean_narasi = stopword_remover.remove(stemmed_narasi)
+
+    # Membagi narasi menjadi kata-kata
+    words = clean_narasi.split()
+
+    # Memilih dua kata yang berbeda secara acak
+    selected_words = []
+
+    if len(words) > 1:
+        # Memilih dua kata secara acak
+        selected_words = random.sample(words, 2)
+    elif len(words) == 1:
+        # Jika hanya terdapat satu kata, duplikat kata tersebut
+        selected_words = [words[0], words[0]]
+    else:
+        # Jika tidak ada kata dalam narasi, berikan nilai kosong
+        selected_words = ["", ""]
+
+    return ' '.join(selected_words)
 
 
 def train_model():
-    datasets = pd.read_csv(DATA_LATIH)
+    datasets = pd.read_csv(data_train)
     print(datasets["label"].value_counts())
 
-    lower = [stemmer.stem(row.lower()) for row in datasets["narasi"]]
-    vectors = [word_tokenize(element) for element in lower]
-    n = 2  # Panjang n-gram yang diinginkan
-    ngram_lower = [list(ngrams(tokens, n)) for tokens in vectors]
-    ngram_texts = [" ".join([" ".join(ngram) for ngram in text]) for text in ngram_lower]
-    labels = datasets["label"]
+    # Membagi dataset menjadi subset data latih (train) dan data uji (test) yang akan digunakan dalam pelatihan dan evaluasi model
+    X_train, X_test, y_train, y_test = train_test_split(datasets['narasi'], datasets['label'], test_size=0.2, random_state=42)
 
-    X_train, X_test, y_train, y_test = train_test_split(ngram_texts, labels, test_size=0.3, stratify=labels)
+    # Membentuk model Word2Vec berdasarkan teks yang terdapat dalam X_train
+    sentences = [text.split() for text in X_train]
+    model = Word2Vec(sentences, min_count=1)
 
-    y_train = Encoder.fit_transform(y_train)
-    y_test = Encoder.fit_transform(y_test)
+    # Menghasilkan vektor representasi dari setiap kalimat dalam X_train dan X_test menggunakan model Word2Vec yang telah dibentuk sebelumnya.
+    X_train_word_vectors = np.array(
+        [
+            np.mean(
+                [model.wv[word] for word in sentence.split() if word in model.wv],
+                axis=0
+            )
+            if any(word in model.wv for word in sentence.split())
+            else np.zeros(model.vector_size)
+            for sentence in X_train
+        ]
+    )
 
-    Tfidf_vect.fit(ngram_texts)
+    X_test_word_vectors = np.array(
+        [
+            np.mean(
+                [model.wv[word] for word in sentence.split() if word in model.wv],
+                axis=0
+            )
+            if any(word in model.wv for word in sentence.split())
+            else np.zeros(model.vector_size)
+            for sentence in X_test
+        ]
+    )
+    # Memastikan bahwa jumlah fitur (jumlah dimensi) dalam X_train_word_vectors dan X_test_word_vectors adalah sama sebelum melanjutkan proses selanjutnya.
+    if X_train_word_vectors.shape[1] == X_test_word_vectors.shape[1]:
+        X_train_word_vectors = X_train_word_vectors.astype(np.float32)
+        X_test_word_vectors = X_test_word_vectors.astype(np.float32)
 
-    Train_X_Tfidf = Tfidf_vect.transform(X_train)
-    Test_X_Tfidf = Tfidf_vect.transform(X_test)
+    else:
+        print("Jumlah fitur pada X_train_word_vectors dan X_test_word_vectors berbeda.")
 
-    SVM = svm.SVC(C=1.0, kernel='linear', degree=1, gamma="auto", verbose=True)
-    SVM.fit(Train_X_Tfidf, y_train)
-    predictions_SVM = SVM.predict(Test_X_Tfidf)
-    print("SVM Accuracy Score -> ", accuracy_score(predictions_SVM, y_test)*100)
-    return SVM
+    return X_train_word_vectors, X_test_word_vectors, y_train, y_test
 
 
 def predict(text):
-    preprocessed_text = preprocess_text(text)
-    text_tfidf = Tfidf_vect.transform([preprocessed_text])
-    prediction = SVM.predict(text_tfidf)
-    return prediction[0]
-
-
-def test_model(SVM):
-    datasets = pd.read_csv(DATA_UJI)
-
-    lower = [stemmer.stem(row.lower()) for row in datasets["narasi"]]
-    vectors = [word_tokenize(element) for element in lower]
-    n = 2  # Panjang n-gram yang diinginkan
-    ngram_lower = [list(ngrams(tokens, n)) for tokens in vectors]
-    ngram_texts = [" ".join([" ".join(ngram) for ngram in text]) for text in ngram_lower]
-
-    Test_X_Tfidf = Tfidf_vect.transform(ngram_texts)
-
-    predictions_SVM = SVM.predict(Test_X_Tfidf)
-
-    data = {"ID": list(datasets["ID"]), "prediksi": predictions_SVM}
-    hasil = pd.DataFrame(data, columns=["ID", "prediksi"])
-    hasil.to_csv("./Hasil Uji Model.csv", index=False)
+    classifier = RandomForestClassifier()
+    classifier.fit(X_train_word_vectors, y_train)
+    predicted = classifier.predict(X_test_word_vectors)
+    return predicted
 
 
 # Train Machine Learning model
-SVM = train_model()
+X_train_word_vectors, X_test_word_vectors, y_train, y_test = train_model()
 
 # Streamlit app
+import streamlit as st
+
 st.title("Deteksi Sentimen Berita")
 st.write("Aplikasi ini digunakan untuk melakukan prediksi sentimen pada teks berita.")
 
@@ -102,7 +120,7 @@ input_text = st.text_area("Masukkan teks berita")
 if st.button("Prediksi"):
     if input_text.strip() != "":
         prediction = predict(input_text)
-        sentiment = Encoder.inverse_transform([prediction])[0]
+        sentiment = prediction[0]
         st.success(f"Hasil Prediksi: {sentiment}")
     else:
         st.warning("Masukkan teks berita untuk melakukan prediksi.")
